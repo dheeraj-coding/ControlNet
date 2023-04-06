ad5fe109ad7f64eab0a06f64e5d037eed1fbca8f
Dheeraj <31967125+dheeraj-coding@users.noreply.github.com>
2023-04-06 12:58:51 -0700

    Fix key indexing for conds

--- cldm/cldm.py
+++ cldm/cldm.py
@@ -385,9 +385,9 @@ class ControlLDM(LatentDiffusion):
 
         log = dict()
         z, c = self.get_input(batch, self.first_stage_key, bs=N)
-        c_cat, c = c["c_concat"][0][:N], c["c_crossattn"][0][:N]
         c_edited = c["edited"][:N]
-        c_inp_embed = c["inp_embed"][:N]
+        c_inp_embed = c["inp_embed"][0][:N]
+        c_cat, c = c["c_concat"][0][:N], c["c_crossattn"][0][:N]
         N = min(z.shape[0], N)
         n_row = min(z.shape[0], n_row)
         log["reconstruction"] = self.decode_first_stage(z)
